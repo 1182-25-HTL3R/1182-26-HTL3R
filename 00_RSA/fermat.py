225c26203121416e45f004614e2f29d7012cebfb
Fabian Ha <1182@htl.rennweg.at>
2025-09-07 11:55:59 +0200

    A.1.1: Schreiben der doctests. Verbesserung der Ausgabe.

--- 00_RSA/fermat.py
+++ 00_RSA/fermat.py
@@ -1,5 +1,6 @@
 __author__ = 'Fabian Ha'
 
+import doctest
 from collections import Counter
 
 
@@ -8,6 +9,13 @@ def fermat(n: int) -> Counter:
     calculates the Fermat numbers for a whole number
     :param n: the whole number greater than 1
     :return: a Counter Object of Fermat numbers
+
+    >>> counter = fermat(2); counter.most_common()
+    [(1, 1)]
+    >>> counter = fermat(9); counter.most_common()
+    [(1, 2), (4, 2), (0, 2), (7, 2)]
+    >>> counter = fermat(569); counter.most_common()
+    [(1, 568)]
     """
     l = []
     if n < 2:
@@ -18,17 +26,19 @@ def fermat(n: int) -> Counter:
     return Counter(l)
 
 
-for p in [2, 3, 5, 7, 11, 997, 9, 15, 21]:
-    c = fermat(p)
-    print(
-        f'{p} -> {c.get(1) * 100 / c.total():.0f} % -> res[1]={c.get(1)}, len(res)={len(c.keys())} - {list(c.items())}')
+if __name__ == '__main__':
+    doctest.testmod(verbose=True)
+    for p in [2, 3, 5, 7, 11, 997, 9, 15, 21]:
+        c = fermat(p)
+        print(
+            f'{p} -> {int(c.get(1) * 100 / c.total())} % -> res[1]={c.get(1)}, len(res)={len(c.keys())} - {c.most_common()}')
 
-for p in range(551, 570):
-    c = fermat(p)
-    print(
-        f'{p} -> {c.get(1) * 100 / c.total():.0f} % -> res[1]={c.get(1)}, len(res)={len(c.keys())} - {list(c.items())}')
+    for p in range(551, 570):
+        c = fermat(p)
+        print(
+            f'{p} -> {int(c.get(1) * 100 / c.total())} % -> res[1]={c.get(1)}, len(res)={len(c.keys())} - {c.most_common()}')
 
-for p in [6601, 8911]:
-    c = fermat(p)
-    print(
-        f'{p} -> {c.get(1) * 100 / c.total():.0f} % -> res[1]={c.get(1)}, len(res)={len(c.keys())} - {list(c.items())}')
+    for p in [6601, 8911]:
+        c = fermat(p)
+        print(
+            f'{p} -> {int(c.get(1) * 100 / c.total())} % -> res[1]={c.get(1)}, len(res)={len(c.keys())} - {c.most_common()}')
