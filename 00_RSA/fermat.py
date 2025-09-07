ad5e2378c9616dcf73728bdb1943a52d44fc2771
Fabian Ha <1182@htl.rennweg.at>
2025-09-07 11:45:08 +0200

    A.1.1: Berechnen der Nicht-Primzahlen und Formatieren der Ausgabe. Kleine Veränderung in fermat Methode.

--- 00_RSA/fermat.py
+++ 00_RSA/fermat.py
@@ -1,21 +1,34 @@
 __author__ = 'Fabian Ha'
 
-def fermat(n: int) -> list:
+from collections import Counter
+
+
+def fermat(n: int) -> Counter:
     """
     calculates the Fermat numbers for a whole number
     :param n: the whole number greater than 1
-    :return: a list of Fermat numbers
+    :return: a Counter Object of Fermat numbers
     """
     l = []
     if n < 2:
         raise ValueError('n must be greater than 1')
     for i in range(1, n):
-        value = pow(i, n-1) % n
-        l.append(value)
+        l.append(pow(i, n - 1) % n)
 
-    return l
+    return Counter(l)
 
-for p in [2, 3, 5, 7, 11]:
-    print(fermat(p))
 
-print(fermat(997))
+for p in [2, 3, 5, 7, 11, 997, 9, 15, 21]:
+    c = fermat(p)
+    print(
+        f'{p} -> {c.get(1) * 100 / c.total():.0f} % -> res[1]={c.get(1)}, len(res)={len(c.keys())} - {list(c.items())}')
+
+for p in range(551, 570):
+    c = fermat(p)
+    print(
+        f'{p} -> {c.get(1) * 100 / c.total():.0f} % -> res[1]={c.get(1)}, len(res)={len(c.keys())} - {list(c.items())}')
+
+for p in [6601, 8911]:
+    c = fermat(p)
+    print(
+        f'{p} -> {c.get(1) * 100 / c.total():.0f} % -> res[1]={c.get(1)}, len(res)={len(c.keys())} - {list(c.items())}')
