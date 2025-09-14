df264b195aabe0e044525b36b2e2ac3c8909f279
Fabian Ha <1182@htl.rennweg.at>
2025-09-14 08:38:39 +0200

    A.1.2: Implementierung der is_prime Funktion.

--- 00_RSA/miller_rabin.py
+++ 00_RSA/miller_rabin.py
@@ -2,6 +2,29 @@ __author__ = 'Fabian Ha'
 
 import random
 
+
+def is_prime(number):
+    if number < 2:
+        return False
+
+    first_hundred_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31,
+                            37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79,
+                            83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137,
+                            139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
+                            197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257,
+                            263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317,
+                            331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389,
+                            397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457,
+                            461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523,
+                            541]
+
+    for prime in first_hundred_primes:
+        if number % prime == 0 and number / prime == 1:
+            return True
+
+    return True if is_prim_millerabin(number, 20) == 'probably prime' else False
+
+
 def is_prim_millerabin(n, k):
     if n <= 3:
         raise ValueError("n must be greater than 3")
@@ -18,19 +41,18 @@ def is_prim_millerabin(n, k):
         a = random.randint(2, n - 2)
         y = pow(a, d, n)
 
-        if y == 1 or y == n-1:
+        if y == 1 or y == n - 1:
             continue
 
         for _ in range(r - 1):
             y = pow(y, 2, n)
             if y == n - 1:
                 break
-        else:               # different approach than in instructions because named loops don't exist in python
+        else:  # different approach than in instructions because named loops don't exist in python
             return "composite"
     return "probably prime"
 
+
 if __name__ == '__main__':
     for i in range(4, 1000):
-        s = is_prim_millerabin(i, 20)
-        if s == "probably prime":
-            print(i)
+        print(f'{i} -> {is_prime(i)}')
