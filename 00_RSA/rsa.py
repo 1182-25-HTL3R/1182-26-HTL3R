840af891c06c13cf05706202a2af9b0862a46457
Fabian Ha <1182@htl.rennweg.at>
2025-09-28 14:58:24 +0200

    A.5: Schreiben des Argparsers und Implementierung der save_keys Funktion

--- 00_RSA/rsa.py
+++ 00_RSA/rsa.py
@@ -1,8 +1,11 @@
 import math
 import random
+import logging
 from typing import Tuple, Generator
 
 from miller_rabin import generate_prime
+
+logger = logging.getLogger("mylogger")
 
 
 def generate_keys(number_of_bits: int) -> Tuple[Tuple[int, int, int], Tuple[int, int, int]]:
@@ -41,12 +44,55 @@ def file2ints(file_path: str, bytenumber: int) -> Generator[int]:
             yield int.from_bytes(block)
 
 
-if __name__ == "__main__":
-    public_key, private_key = generate_keys(128)
-    for x in [1, 15, 289, 1044]:
-        c = pow(x, public_key[0], public_key[1])
-        y = pow(c, private_key[0], private_key[1])
-    assert x == y
+def save_keys(keys: Tuple[Tuple[int, int, int], Tuple[int, int, int]]) -> None:
+    """
+    saves the generated keys in files "public_key.txt" and "private_key.txt"
+    :param keys: Tuple of public and private keys
+    :return: None
+    """
+    with open("public_key.txt", "w") as f:
+        f.write(str(keys[0][0]))
+        f.write("\n")
+        f.write(str(keys[0][1]))
 
-    ints = file2ints("message.txt", 8)
-    for i in ints: print(i)
+    logger.info("Public key saved to public_key.txt")
+
+    with open(f"private_key.txt", "w") as f:
+        f.write(str(keys[1][0]))
+        f.write("\n")
+        f.write(str(keys[1][1]))
+
+    logger.info("Private key saved to private_key.txt")
+
+
+if __name__ == "__main__":
+    # public_key, private_key = generate_keys(128)
+    # for x in [1, 15, 289, 1044]:
+    #     c = pow(x, public_key[0], public_key[1])
+    #     y = pow(c, private_key[0], private_key[1])
+    # assert x == y
+    #
+    # ints = file2ints("message.txt", 8)
+    # for i in ints: print(i)
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Fabians RSA Verschlüsselungs Tool")
+    parser.add_argument("-l", "--loglevel", help="Set the logging level", default="WARNING",
+                        choices=["DEBUG", "INFO", "WARNING", "ERROR"], required=False)
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-k", "--keygen", metavar="KEYGEN", help="generate new keys with the given length", type=int)
+    group.add_argument("-e", "--encrypt", metavar="ENCRYPT", help="encrypt file", type=str)
+    group.add_argument("-d", "--decrypt", metavar="DECRYPT", help="decrypt file", type=str)
+    args = parser.parse_args()
+
+    if args.loglevel:
+        logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=args.loglevel)
+        logger.setLevel(args.loglevel)
+
+    if args.keygen:
+        logger.info(f"Generating new keys with length {args.keygen}")
+        generated_keys = generate_keys(args.keygen)
+        save_keys(generated_keys)
+        logger.info("Done generating keys")
