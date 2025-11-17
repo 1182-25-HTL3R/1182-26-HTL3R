9fdd36fcbdb7d83795e47038af1706810397542a
Fabian Ha <1182@htl.rennweg.at>
2025-11-17 21:57:53 +0100

    Hinzufügen der Beobachtungen / Ergebnisse

--- 04_Labyrinth/a_star.py
+++ 04_Labyrinth/a_star.py
@@ -12,19 +12,22 @@ def load_maze(s):
                 maze[(i, j)] = line[j]
     return maze
 
+
 directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 next_positions = [(0, 0)]
+
 
 def h(cell1, cell2):
     x1, y1 = cell1
     x2, y2 = cell2
     return abs(x1 - x2) + abs(y1 - y2)
 
+
 def a_star(maze, start, goal: tuple):
-    g_score = {(cell[0], cell[1]):float("inf") for cell in maze.keys()}
+    g_score = {(cell[0], cell[1]): float("inf") for cell in maze.keys()}
     g_score[start] = 0
-    f_score = {(cell[0], cell[1]):float("inf") for cell in maze.keys()}
+    f_score = {(cell[0], cell[1]): float("inf") for cell in maze.keys()}
     f_score[start] = h(start, goal)
 
     open = [(h(start, goal), h(start, goal), start)]
@@ -60,6 +63,7 @@ def a_star(maze, start, goal: tuple):
 
     return forwardPath
 
+
 from timer import time_function
 
 if __name__ == "__main__":
@@ -91,3 +95,61 @@ if __name__ == "__main__":
     print("\nLabyrinth 6")
     maze6 = load_maze("l6.txt")
     time_function(a_star(maze6, (0, 280), goal), False)
+
+    '''
+    Tiefensuche (Koordinaten-Tuple: (X, Y)):
+        Labyrinth 1
+        [(1, 1), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7), (8, 7), (9, 7), (10, 7), (10, 8), (10, 9), (9, 9), (9, 10), (8, 9), (7, 9), (7, 10), (6, 10), (6, 11)]
+        Ausgang bei: (6, 11)
+        Dauerte 10.01358 Millisekunden
+        
+        Labyrinth 2
+        Ausgang bei: (29, 30)
+        Dauerte 3.8147 Millisekunden
+        
+        Labyrinth 3
+        Ausgang bei: (199, 200)
+        Dauerte 9.05991 Millisekunden
+        
+        Labyrinth 4
+        Ausgang bei: (298, 200)
+        Dauerte 14.78195 Millisekunden
+        
+        Labyrinth 5
+        Ausgang bei: (1, 200)
+        Dauerte 10.96725 Millisekunden
+        
+        Labyrinth 6
+        Ausgang bei: (280, 0)
+        Dauerte 5.00679 Millisekunden
+    
+    A* Algorithm (Koordinaten-Tuple: (Y, X)) :
+        Labyrinth 1
+        [(1, 1), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2), (7, 1), (8, 1), (9, 1), (10, 1), (10, 2), (10, 3), (9, 3), (9, 4), (9, 5), (10, 5), (10, 6), (11, 6)]
+        Ausgang bei: (11, 6)
+        Dauerte 3.09944 Millisekunden
+        
+        Labyrinth 2
+        Ausgang bei: (30, 29)
+        Dauerte 5.96046 Millisekunden
+        
+        Labyrinth 3
+        Ausgang bei: (200, 199)
+        Dauerte 16.92772 Millisekunden
+        
+        Labyrinth 4
+        Ausgang bei: (200, 298)
+        Dauerte 20.98083 Millisekunden
+        
+        Labyrinth 5
+        Ausgang bei: (200, 1)
+        Dauerte 6.19888 Millisekunden
+        
+        Labyrinth 6
+        Ausgang bei: (0, 280)
+        Dauerte 11.92093 Millisekunden
+    
+    -> Beobachtung: 
+        - A* meist langsamer, da der kürzeste Weg gefunden wird während Tiefensuche irgendeinen Weg zum Ziel findet und durch Zufall/Glück schnell einen richtigen Weg wählt.
+        - Beide Algorithmen haben ihre Vorteile, und werden in verschiedenen Szenarien präferiert.
+    '''
